d736c6d68b0f1f469f107c006a1e0b13da4d7255
Lukas Buivydas <lukan@Grifu-Baze.lan>
2023-04-27 20:24:34 +0300

    Filmo projektas

--- 1G API-project.py
+++ 1G API-project.py
@@ -1,2 +1,14 @@
-print("----")
+import requests
+import json
 
+filmo_pav = input("Įveskite filmo pavadinimą: ")
+url = "http://www.omdbapi.com/?apikey=4a008e36&s=" + filmo_pav
+
+response = requests.get(url)
+duomenys = json.loads(response.text)
+
+if duomenys["Response"] == "False":
+    print("Filmas nerastas.")
+else:
+    for filmai in duomenys["Search"]:
+        print(filmai["Title"], "(", filmai["Year"], ")")
