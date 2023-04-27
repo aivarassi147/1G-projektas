2d9a3005543416e6c5460444e2c3c4598de5280e
Aidas8 <131284077+Aidas8@users.noreply.github.com>
2023-04-27 21:04:13 +0300

    Update 1G API-project.py

--- 1G API-project.py
+++ 1G API-project.py
@@ -12,3 +12,27 @@ if duomenys["Response"] == "False":
 else:
     for filmai in duomenys["Search"]:
         print(filmai["Title"], "(", filmai["Year"], ")")
+        
+       
+    
+    
+  ------------------------------------------------
+from tkinter import *
+langas = Tk()
+
+
+def spausdinti():
+    ivesta = laukas1.get()
+    rezultatas["text"] = f"visi filmai su tavo zodziu"
+
+uzrasas1 = Label(langas, text="ivesk zodi")
+laukas1 = Entry(langas)
+mygtukas = Button(langas, text="ziurim ka turim", command=spausdinti)
+rezultatas =Label(langas, text="")
+
+uzrasas1.grid(row=0, column=0)
+laukas1.grid(row=0, column=1)
+mygtukas.grid(row=0, column=2)
+rezultatas.grid(row=1, columnspan=3)
+
+langas.mainloop()
